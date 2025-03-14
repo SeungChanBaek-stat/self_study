7c8942ffabe1d09b486d5c2c336d3a98ec7cff54
SeungChanBaek-stat <drbsc123@gmail.com>
2025-03-14 16:55:02 +0900

    adv_reg chapter 1,2,3

---
+++ adv_reg/exercise_sheet.R
@@ -0,0 +1,11 @@
+112/75
+qt(0.05, df = 63, lower.tail = TRUE)
+pnorm(-1.5,lower.tail = TRUE)
+pnorm(-0.5, lower.tail = TRUE) - pnorm(-1.5, lower.tail = TRUE)
+pnorm(0.5, lower.tail = TRUE) - pnorm(-0.5, lower.tail = TRUE)
+
+1 - (pnorm(sqrt(5), lower.tail = TRUE) - pnorm(-sqrt(5), lower.tail = TRUE) )
+
+1 - (pnorm(-sqrt(5) * (0.5), lower.tail = TRUE) - pnorm(-sqrt(5) * (2.5), lower.tail = TRUE) )
+
+16*13 - 9 * 15
