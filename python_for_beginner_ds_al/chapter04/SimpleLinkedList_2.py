e1aa35c332e392bb5574af3aa4e6c920e04b3942
SeungChanBaek-stat <drbsc123@gmail.com>
2025-03-15 20:13:17 +0900

    study

--- python_for_beginner_ds_al/chapter04/SimpleLinkedList_2.py
+++ python_for_beginner_ds_al/chapter04/SimpleLinkedList_2.py
@@ -1,3 +1,8 @@
+# import sys, os
+# sys.path.append("..")
+# from functions.SimpleLinkedList import Node, printNodes, insertNode_
+
+
 class Node():
     def __init__(self):
         self.data = None
@@ -13,13 +18,6 @@ def printNodes(start):
         current = current.link
         print(current.data, end = ' ')
     print()
-
-def insertNode(findData, insertData):
-    global memory, head, current, pre
-
-    if head.data == findData:
-        node = Node()
-        node.data = insertData
 
 def insertNode_(findData, insertData):
     global memory, head, current, pre
@@ -45,3 +43,37 @@ def insertNode_(findData, insertData):
     node = Node()   # 마지막 노드 삽입
     node.data = insertData
     current.link = node
+
+
+## 전역 변수 선언 부분 ##
+memory = []
+head, current, pre = None, None, None
+dataArray = ['다현', '정연', '쯔위', '사나', '지효']
+
+## 메인 코드 부분 ##
+if __name__ == "__main__":
+
+    node = Node()       # 첫 번째 노드
+    node.data = dataArray[0]
+    head = node
+    memory.append(node)
+
+    for data in dataArray[1:]:      # 두 번째 이후 노드
+        pre = node
+        node = Node()
+        node.data = data
+        pre.link = node
+        memory.append(node)
+    
+    printNodes(head)
+
+    insertNode_("다현", "화사")
+    printNodes(head)
+
+    insertNode_("사나", "솔라")
+    printNodes(head)
+
+    insertNode_("재남", "문별")
+    printNodes(head)
+
+    
