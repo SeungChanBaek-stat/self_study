27c54f2f5343fe8f9e9b7f039e4b6bf8d60f5984
SeungChanBaek-stat <drbsc123@gmail.com>
2025-04-02 01:44:44 +0900

    chapter 11

--- adv_reg/Hitters_var_selection.R
+++ adv_reg/Hitters_var_selection.R
@@ -11,18 +11,6 @@ hit.dat <- Hitters %>% na.omit %>% select(Salary, AtBat, Hits, HmRun, Runs, RBI,
 
 X = hit.dat[, -c(1)] ; y = hit.dat[, c(1)]
 
-# print(class(X))
-# print(class(y))
-# p = dim(X)[2]
-# 
-# for (i in 1:p){
-#   cat(i,"번째 열의 클래스 : ",  class(X[,i]))
-# }
-# class(X[,2])
-
 res_hit = best_subset(X, y)
-
-# y
-# class(y)
 
 
